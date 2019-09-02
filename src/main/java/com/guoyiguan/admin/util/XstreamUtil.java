ef49fed74248c0d51859de87ddcc9ced91121a8a
zhangyj <zhangyj3@guahao.com>
2019-09-02 16:27:26 +0800

    优化

--- src/main/java/com/guoyiguan/admin/util/XstreamUtil.java
+++ src/main/java/com/guoyiguan/admin/util/XstreamUtil.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2001-2019 GuaHao.com Corporation Limited. All rights reserved.
- * This software is the confidential and proprietary information of GuaHao Company.
- * ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use it only
- * in accordance with the terms of the license agreement you entered into with GuaHao.com.
- */
 package com.guoyiguan.admin.util;
 
 import com.guoyiguan.admin.bean.WecharMessageTextEntity;
